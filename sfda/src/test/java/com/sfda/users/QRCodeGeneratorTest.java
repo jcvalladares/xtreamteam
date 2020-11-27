90810e55f50caf00eb233534343e3b0f1ff180db
Anup Shetye <anup.shetye@gmail.com>
2020-11-27 14:03:22 -0500

    initial test cases...planning to use ZXIng (Zebra Crossing) library to generate QR codes

--- sfda/src/test/java/com/sfda/users/QRCodeGeneratorTest.java
+++ sfda/src/test/java/com/sfda/users/QRCodeGeneratorTest.java
@@ -1,7 +1,18 @@
 package com.sfda.users;
 
 import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.junit.jupiter.api.Test;
+
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
+import com.sfda.util.QRCodeGenerator;
 
 class QRCodeGeneratorTest {
 
@@ -11,8 +22,14 @@ class QRCodeGeneratorTest {
     }
 
 	@Test
-    public void testQRCodeIsNotEmpty(){
-		assertTrue(true);
+    public void testQRCodeIsNotEmpty() throws WriterException, IOException{
+		QRCodeGenerator codeGenerator = new QRCodeGenerator();
+		String qrGeneratorCode = "SFDA_Test";
+		String charset = "UTF-8"; 
+		Map<EncodeHintType, ErrorCorrectionLevel> hintMap = new HashMap<EncodeHintType, ErrorCorrectionLevel>();
+		hintMap.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.L);
+		BitMatrix matrix = codeGenerator.createQRCode(qrGeneratorCode, charset, hintMap, 100, 100);
+		assertTrue(matrix != null);
     }
 	
 	@Test
