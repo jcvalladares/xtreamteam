104420bf412d628c294d1b9aab9f78dbf673cda0
Anup Shetye <anup.shetye@gmail.com>
2020-11-27 13:28:25 -0500

    new JUnits for QR generator and User Validations

---
+++ sfda/src/test/java/com/sfda/users/QRCodeGeneratorTest.java
@@ -0,0 +1,32 @@
+package com.sfda.users;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.jupiter.api.Test;
+
+class QRCodeGeneratorTest {
+
+	@Test
+    public void junitWorksProperly(){
+		assertTrue(true);
+    }
+
+	@Test
+    public void testQRCodeIsNotEmpty(){
+		assertTrue(true);
+    }
+	
+	@Test
+    public void testQRCodeValidtity(){
+		assertTrue(true);
+    }
+	
+	@Test
+    public void testQRCodeImageFileWasCreated(){
+		assertTrue(true);
+    }
+	
+	@Test
+    public void testQRCodeInputParamsValidity(){
+		assertTrue(true);
+    }
+}
---
+++ sfda/src/test/java/com/sfda/users/SFDAUserValidatorTest.java
@@ -0,0 +1,32 @@
+package com.sfda.users;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.jupiter.api.Test;
+
+class SFDAUserValidatorTest {
+
+	@Test
+    public void junitWorksProperly(){
+		assertTrue(true);
+    }
+
+	@Test
+    public void testValidateIfTrustedDonor(){
+		assertTrue(true);
+    }
+	
+	@Test
+    public void testValidateIfAdultUser(){
+		assertTrue(true);
+    }
+	
+	@Test
+    public void testVaidateIfUserRegistrationComplete(){
+		assertTrue(true);
+    }
+	
+	@Test
+    public void testValidateIfUserIsHeadOfFamily(){
+		assertTrue(true);
+    }
+}
