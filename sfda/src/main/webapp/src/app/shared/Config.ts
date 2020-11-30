571d289b52dd4b54117ad9411dbf2596c6455612
Juan Valladares <jcvalladares@americasgroup.net>
2020-11-30 13:00:42 -0600

    temporarly eliminating the qrcode

--- sfda/src/main/webapp/src/app/shared/Config.ts
+++ sfda/src/main/webapp/src/app/shared/Config.ts
@@ -2,8 +2,9 @@ import { Injectable } from '@angular/core';
 Injectable();
 export class Config
 {
-  public serverUrl = 'http://localhost:8080/';
+  public serverUrl = 'http://54.219.4.96:8080/';
   //public serverUrl = '/';
   public loginUrl = this.serverUrl + 'api/login/login';
   public signupUrl = this.serverUrl + 'api/login/signup';
+  public getQrCodeUrl = this.serverUrl + 'get_qr?email=test@test.com'; // todo: fix it
 }
