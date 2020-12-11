b8f9a5b2f86731cf71ba0480e798164171aec8cb
Anup Shetye <anup.shetye@gmail.com>
2020-12-10 22:37:57 -0500

    added new test cases - initial version

---
+++ sfda/src/test/java/com/sfda/users/UserRegistrationTest.java
@@ -0,0 +1,38 @@
+package com.sfda.users;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class UserRegistrationTest {
+	
+	@Test
+	public void junitWorksProperly() {
+		assertTrue(true);
+	}
+	
+	@Test
+	public void testValidPassword() {
+		assertTrue(true);
+	}
+	
+	@Test
+	public void testValidFirstNameLastName() {
+		assertTrue(true);
+	}
+	
+	@Test
+	public void testSqlInjections() {
+		assertTrue(true);
+	}
+	
+	@Test
+	public void testValidBirthDate() {
+		assertTrue(true);
+	}
+	
+	@Test
+	public void test() {
+		assertTrue(true);
+	}
+}
