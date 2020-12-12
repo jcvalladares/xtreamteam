0cee6127ccae6a5bf9869d3f8ee64dadf0c1adac
Juan Valladares <jcvalladares@americasgroup.net>
2020-12-12 16:43:26 -0600

    initial version signup

--- sfda/src/main/webapp/src/app/singup/signup.service.ts
+++ sfda/src/main/webapp/src/app/singup/signup.service.ts
@@ -1,5 +1,6 @@
+import { Singup } from './singup';
 import { Config } from '../shared/Config';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { IUser } from '../shared/IUser';
@@ -7,12 +8,13 @@ import { IUser } from '../shared/IUser';
   providedIn: 'root'
 })
 export class SignupService {
-  public signUp(user: IUser ): Observable<IUser> {
-    const body = JSON.stringify( user);
-    const headers = new Headers({ 'Content-Type': 'application/json'});
-    const options = {body, Headers: headers};
-    return this.http.post<IUser>(this.config.signupUrl, options);
-
+  public signUp(user: Singup ): Observable<Singup> {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json'
+      })
+    };
+    return this.http.post<Singup>(this.config.signupUrl, user,  httpOptions);
      }
   constructor(private http: HttpClient, private config: Config) { }
 }
--- sfda/src/main/webapp/src/app/singup/singup.component.ts
+++ sfda/src/main/webapp/src/app/singup/singup.component.ts
@@ -1,10 +1,11 @@
 import { SignupService } from './signup.service';
 import { IUser } from './../shared/IUser';
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormControl, FormBuilder, Validators, ValidatorFn, ValidationErrors } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { Config } from '../shared/Config';
-
+import { Singup } from './singup';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-singup',
   templateUrl: './singup.component.html',
@@ -12,29 +13,42 @@ import { Config } from '../shared/Config';
 })
 export class SingupComponent implements OnInit {
 
-
   signUpForm = new FormGroup({
-    firstName: new FormControl(''),
+    TYPE: new FormControl('',[Validators.required]),
+    firstName: new FormControl('', [Validators.required]),
     middleName: new FormControl(''),
-    lastName: new FormControl(''),
-    Id: new FormControl(''),
-    phone: new FormControl(''),
-    email: new FormControl(''),
-    password: new FormControl(''),
+    lastName: new FormControl('', [Validators.required]),
+    phone: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required]),
     password2: new FormControl('')
-  });
 
-  onSubmit(newUser: IUser) {
+  }, {validators: passwordValidator});
+  get firstName() {return this.signUpForm.get("firstName");}
+  get lastName() {return this.signUpForm.get("lastName");}
+  get phone() {return this.signUpForm.get("phone");}
+  get email() {return this.signUpForm.get("email");}
+  get password() {return this.signUpForm.get("password");}
+  get password2() {return this.signUpForm.get("password2");}
+
+  onSubmit(newUser: any) {
+
     this.http.signUp(newUser)
-    .subscribe((token: IUser) => {
-
+    .subscribe((user: Singup) => {
+        console.log(user);
     }, (error) => {
-
+      console.log(error);
     });
-
   }
+
   constructor(private http: SignupService, private config: Config) { }
   ngOnInit(): void {
   }
 
 }
+
+export const passwordValidator: ValidatorFn = (control: FormGroup): ValidationErrors | null => {
+  const password = control.get('password');
+  const password2 = control.get('password2');
+  return password && password2 && password.value !== password2.value ? { identityRevealed: true } : null;
+};
