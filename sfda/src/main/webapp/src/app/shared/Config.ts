2cbfe7c8f68ea577f4aab315eca735d547cc7073
Juan Valladares <jcvalladares@americasgroup.net>
2020-12-13 14:37:35 -0600

    error config file

--- sfda/src/main/webapp/src/app/shared/Config.ts
+++ sfda/src/main/webapp/src/app/shared/Config.ts
@@ -2,8 +2,8 @@ import { Injectable } from '@angular/core';
 Injectable();
 export class Config
 {
-//  public serverUrl = 'http://54.219.4.96:8080/';
-  public serverUrl = 'http://localhost:8080/';
+   public serverUrl = 'http://54.219.4.96:8080/';
+  //public serverUrl = 'http://localhost:8080/';
   public loginUrl = this.serverUrl + 'api/login/login';
   public signupUrl = this.serverUrl + 'api/user';
   public getQrCodeUrl = this.serverUrl + 'api/get_qr?';
